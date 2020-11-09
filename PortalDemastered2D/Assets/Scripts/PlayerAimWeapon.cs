091bbf8d0b975937cfaebab31c9b8a3490b17790
Bizarre122 <66393770+Bizarre122@users.noreply.github.com>
2020-11-09 14:09:33 -0800

    Malarkey.

--- PortalDemastered2D/Assets/Scripts/PlayerAimWeapon.cs
+++ PortalDemastered2D/Assets/Scripts/PlayerAimWeapon.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
 
 public class PlayerAimWeapon : MonoBehaviour
 {
+
+    public event EventHandler<OnShootEventArgs> OnShoot;
+    public class OnShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;
+        public Vector3 shootPosition;
+    }
+
     private Transform aimTransform;
+    private Transform aimGunEndPointTransform;
+    private Animator aimAnimator;
 
     private void Awake()
     {
         aimTransform = transform.Find("Aim");
+        aimAnimator = aimTransform.GetComponent<Animator>();
+        aimGunEndPointTransform = aimTransform.Find("GunEndPointPosition");
     }
 
     private void Update()
+    {
+        HandleAiming();
+        HandleShooting();
+    }
+
+    private void HandleAiming()
     {
         var mousePos = Input.mousePosition;
         mousePos.z = 10;
@@ -21,5 +40,23 @@ public class PlayerAimWeapon : MonoBehaviour
         Vector3 aimDirection = (mousePosition - transform.position).normalized;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 10;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+            aimAnimator.SetTrigger("Shoot");
+            OnShoot?.Invoke(this, new OnShootEventArgs
+            {
+                gunEndPointPosition = aimGunEndPointTransform.position,
+                shootPosition = mousePosition,
+            });
+        }
     }
 }
---
+++ PortalDemastered2D/Assets/Testing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils; 
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField] private PlayerAimWeapon playerAimWeapon;
+
+    private void Start()
+    {
+        playerAimWeapon.OnShoot += PlayerAimWeapon_OnShoot;
+    }
+
+    private void PlayerAimWeapon_OnShoot(object sender, PlayerAimWeapon.OnShootEventArgs e)
+    {
+        UtilsClass.ShakeCamera(1f, .2f);
+        WeaponTracer.Create(e.gunEndPointPosition, e.shootPosition);
+        Shoot_Flash.AddFlash(e.gunEndPointPosition);
+    }
+}
