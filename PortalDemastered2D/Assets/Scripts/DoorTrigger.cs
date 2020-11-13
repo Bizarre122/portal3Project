168a4c0b1a83ef891cdcc368131f6a60c0cdcb84
Bizarre122 <66393770+Bizarre122@users.noreply.github.com>
2020-11-13 14:44:15 -0800

    Working Script for deactivating door.

--- PortalDemastered2D/Assets/Scripts/DoorTrigger.cs
+++ PortalDemastered2D/Assets/Scripts/DoorTrigger.cs
@@ -7,4 +7,21 @@ public class DoorTrigger : MonoBehaviour
     private int wilam = 14;
     public GameObject door;
     
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Pressure Switch")
+        {
+            door.SetActive(false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Pressure Switch")
+        {
+            door.SetActive(true);
+        }
+    }
+
 }
