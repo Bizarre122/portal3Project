7be96af06190ad5b582f72c22c5546750015eed4
brentb0803 <65979328+brentb0803@users.noreply.github.com>
2020-11-13 14:04:06 -0800

    forWillam
    for WIlamaiam

---
+++ PortalDemastered2D/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private int wilam = 14;
+    public GameObject door;
+    
+}
