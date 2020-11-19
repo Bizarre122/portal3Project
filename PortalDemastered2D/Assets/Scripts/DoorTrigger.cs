85f445b8fa3fb628b5145a1616cb7cf1d00830e4
Bizarre122 <66393770+Bizarre122@users.noreply.github.com>
2020-11-19 14:13:22 -0800

    Awesome

---
+++ PortalDemastered2D/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private int wilam = 14;
+    public GameObject door;
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Pressure Switch")
+        {
+            door.SetActive(false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Pressure Switch")
+        {
+            door.SetActive(true);
+        }
+    }
+
+}
